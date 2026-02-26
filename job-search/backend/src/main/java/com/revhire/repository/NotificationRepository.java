7d2ea376e3e4f8c9249ef9e06939f4f42255a6a5
anilkumar222222 <anilkumar.vemu.cse@gmail.com>
2026-02-27 00:46:20 +0530

    merge changes taken

---
+++ job-search/backend/src/main/java/com/revhire/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package com.revhire.repository;
+
+import com.revhire.entity.Notification;
+import com.revhire.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByRecipientOrderByCreatedAtDesc(User recipient);
+    long countByRecipientAndIsReadFalse(User recipient);
+}
